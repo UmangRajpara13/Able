d5e5bede11e822d7d4441b3d9b910283cce8fb41
Umang Rajpara <umangrajpara99@gmail.com>
2023-01-10 03:05:23 +0100

    integrated in one repo

---
+++ whisper-mint/websocketInterface.py
@@ -0,0 +1,13 @@
+import asyncio
+from websockets import connect
+
+ws = None
+
+
+async def connectWebSocket(uri, ):
+    global ws
+    ws = await connect(uri)
+
+
+async def send(msg):
+    await ws.send('stt|' + msg)
