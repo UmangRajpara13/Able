d961fa68d10c83f4be426f39464ba80facce38a4
Umang Rajpara <umangrajpara99@gmail.com>
2023-03-31 12:04:29 +0200

    bf

--- stt_only.sh
+++ stt_only.sh
@@ -6,4 +6,8 @@ nvidia-smi | grep python | awk '{print $5}' | cut -d '.' -f 1 | while read pid; 
 done
 
 
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:"/home/user/Desktop/My Projects/able_dev/whisper-mint/whisper/lib/python3.9/site-packages/torch/lib"
+export CUDA_HOME=${CUDA_HOME}:"/home/user/Desktop/My Projects/able_dev/whisper-mint/whisper/lib/python3.9/site-packages/torch/cuda"
+source ~/.zshrc
+
 whisper-mint/whisper/bin/python3 ./whisper-mint/main.py 1111
